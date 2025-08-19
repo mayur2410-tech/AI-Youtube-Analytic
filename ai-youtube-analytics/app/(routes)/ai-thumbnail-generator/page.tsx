dddb4909480d57e5f85f8653231758394501ced7
mayur2410-tech <mayurwaykar9@gmail.com>
2025-08-19 22:32:15 +0530

    add the logic to add and remove the image and to see the preview

--- ai-youtube-analytics/app/(routes)/ai-thumbnail-generator/page.tsx
+++ ai-youtube-analytics/app/(routes)/ai-thumbnail-generator/page.tsx
@@ -1,7 +1,34 @@
-import { ArrowUp, ImagePlus } from 'lucide-react'
-import React from 'react'
+"use client"
+import { ArrowUp, ImagePlus, User, X } from 'lucide-react'
+import Image from 'next/image';
+import React, { useState } from 'react'
 
 function AiThumbnailGenerator() {
+
+    const [userInput, setUserInput] = useState<string>();
+    const [referenceImage, setReferenceImage] = useState<any>();
+    const [faceImage, setFaceImage] = useState<any>();
+    const [referenceImagePreview, setReferenceImagePreview] = useState<any>();
+    const [faceImagePreview, setFaceImagePreview] = useState<any>();
+
+    // console.log("input",userInput)
+    const OnCrossClick =()=>{
+            setReferenceImagePreview(null)
+    }
+
+    const onHandleFileChange = (field: string, e: any) => {
+        const selectedFile = e.target.files[0];
+        if (field == 'referenceImage') {
+            setReferenceImage(selectedFile)
+            setReferenceImagePreview(URL.createObjectURL(selectedFile))
+            console.log('url', URL.createObjectURL(selectedFile))
+        } else {
+            setFaceImage(selectedFile)
+            setFaceImagePreview(URL.createObjectURL(selectedFile))
+        }
+    }
+
+
     return (
         <div>
             <div className='px-10 md:px-20 lg:px-40'>
@@ -13,16 +40,64 @@ function AiThumbnailGenerator() {
                         no design skills needed.</p>
                 </div>
                 <div className='flex gap-5 items-center p-3 border rounded-xl mt-10 bg-secondary'>
-                    <textarea placeholder='Enter your youtube viedo titel or description' className='w-full outline-0 bg-transparent' />
-<div className='p-3 bg-gradient-to-t from-red-500 to-orange-500 rounded-full'>
-    <ArrowUp />
-</div>
-                </div>
-                <div className='mt-3'>
-                    <div className='p-4 w-full border rounded-xl bg-secondary flex gap-2 items-center justify-center'>
-                        <ImagePlus />
-                        <h2>Referance Image</h2>
+                    <textarea placeholder='Enter your youtube video titel or description' className='w-full outline-0 bg-transparent'
+                        onChange={(e) => setUserInput(e.target.value)} />
+                    <div className='p-3 bg-gradient-to-t from-red-500 to-orange-500 rounded-full'>
+                        <ArrowUp />
                     </div>
+                </div>
+                <div className='mt-3 flex gap-5'>
+                    <label htmlFor="refernanceImageUplaod" className='w-full'>
+                        {!referenceImagePreview ?
+                            <div className='p-4 w-full border rounded-xl bg-secondary flex gap-2 items-center justify-center hover:scale-105 transition-all cursor-pointer'>
+                                <ImagePlus />
+                                <h2>Referance Image</h2>
+                            </div> :
+                            <div className="relative inline-block">
+                                {/* Close Icon */}
+                                <button
+                                    onClick={OnCrossClick}
+                                    className="absolute -top-2 -right-2 bg-white rounded-full shadow p-1 hover:bg-gray-100 transition"
+                                >
+                                    <X className='w-4 h-4' />
+                                </button>
+
+                                {/* Image */}
+                                <Image
+                                    src={referenceImagePreview}
+                                    alt="Reference Image"
+                                    width={100}
+                                    height={100}
+                                    className="w-[70px] h-[70px] object-cover rounded-sm"
+                                />
+                            </div>
+
+
+                        }
+                    </label>
+                    <input type='file' id='refernanceImageUplaod' className='hidden'
+                        onChange={(e) => onHandleFileChange('referenceImage', e)} />
+                    <label htmlFor="includeFace" className='w-full'>
+                        {!faceImagePreview ? <div className='p-4 w-full border rounded-xl bg-secondary flex gap-2 items-center justify-center hover:scale-105 transition-all cursor-pointer'>
+                            <User />
+                            <h2>Include Face</h2>
+                        </div> : <div className="relative inline-block">
+                            <button
+                                onClick={() => console.log("Remove image")}
+                                className="absolute -top-2 -right-2 bg-white rounded-full shadow p-1 hover:bg-gray-100 transition"
+                            >
+                                <X className='w-4 h-4' />
+                            </button>
+                            <Image src={faceImagePreview} alt='Referance Image' width={100} height={100} className='w-[70px] h-[70px] object-cover rounded-sm' />
+                        </div>
+
+                        }
+                    </label>
+                    <input type="file" id='includeFace' className='hidden'
+                        onChange={(e) => onHandleFileChange('faceImage', e)} />
+
+
+
                 </div>
             </div>
         </div>
