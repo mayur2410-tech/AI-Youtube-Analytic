8a75979dd0febaa5874c3df5002bb9872a82decb
mayur2410-tech <mayurwaykar9@gmail.com>
2025-08-20 16:35:21 +0530

    add the openAI for creating the prompt to make a thumbnail image accordign to userinput

--- ai-youtube-analytics/inngest/function.ts
+++ ai-youtube-analytics/inngest/function.ts
@@ -1,5 +1,7 @@
 import { inngest } from "./client";
-import ImageKit from 'imagekit' 
+import ImageKit from 'imagekit';; 
+import OpenAI from 'openai';
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -24,6 +26,11 @@ privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
 urlEndpoint:process.env.IMAGEKIT_URLENDPOINT,
 })
 
+export const openai = new OpenAI({
+  baseURL: 'https://openrouter.ai/api/v1',
+  apiKey: process.env.OPENROUTER_API_KEY,
+ 
+});
 
 export const GenerateAiThumbnail = inngest.createFunction(
   {id: 'ai/generate-thumbnail'},
@@ -37,6 +44,8 @@ export const GenerateAiThumbnail = inngest.createFunction(
           async()=>{
          
             if(refImage!=null){
+
+            
 
               const refImageUrl =  await imagekit.upload({
                 file:refImage?.buffer??'',
@@ -55,24 +64,58 @@ export const GenerateAiThumbnail = inngest.createFunction(
   
               // })
   
-              return {
-                refImageUrl:refImageUrl.url,
+              return refImageUrl.url
+             
                 // faceImageUrl:faceImageUrl.url,
   
-              }
             }else{
               return null
             }
+            
+          
           }
         )
 
         // generate ai prompt from ai model 
+
+          const generateThumbnailPrompt= await step.run('generateThumnailPrompt',async()=>{
+            const completion = await openai.chat.completions.create({
+    model: 'google/gemini-2.5-flash',
+    messages: [
+      {
+        role: 'user',
+        content: uploadImageUrls? [
+              {
+                "type": "text",
+                "text": `Referring to this thumbnail URL, write a text prompt to generate a YouTube thumbnail similar to the attached reference image with the following user input `+userInput+'Only give me text prompt, no other comment text'
+              },
+              {
+                "type": "image_url",
+                "image_url": { "url": uploadImageUrls }
+              }
+            ]
+          : [
+              {
+                "type": "text",
+                "text": `Depends on user input, write a text prompt to generate a high-quality professional YouTube thumbnail prompt. Add relevant icons, illustrations, or images as per the title. UserInput `+userInput+'Only give me text prompt, no other comment text'
+              }
+            ]
+          
+      },
+    ],
+    max_tokens:500
+  });
+
+  console.log(completion.choices[0].message.content)
+ return completion.choices[0].message.content
+
+          })
 
         //generate ai image
 
         //save iamge to cloud
 
         //save record to databse
-        return uploadImageUrls
+        return generateThumbnailPrompt
   }
 )
