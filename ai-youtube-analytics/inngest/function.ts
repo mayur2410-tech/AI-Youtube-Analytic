26bdbf3656663f740c1b960a12d685a00b0c368e
mayur2410-tech <mayurwaykar9@gmail.com>
2025-08-20 14:45:20 +0530

    Complete the image upload function to imagekit

--- ai-youtube-analytics/inngest/function.ts
+++ ai-youtube-analytics/inngest/function.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import ImageKit from 'imagekit' 
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -9,6 +10,20 @@ export const helloWorld = inngest.createFunction(
   },
 );
 
+  //@ts-ignore
+
+const imagekit = new ImageKit({
+  //@ts-ignore
+  
+publicKey:process.env.IMAGEKIT_PUBLIC_KEY,
+  //@ts-ignore
+
+privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
+  //@ts-ignore
+
+urlEndpoint:process.env.IMAGEKIT_URLENDPOINT,
+})
+
 
 export const GenerateAiThumbnail = inngest.createFunction(
   {id: 'ai/generate-thumbnail'},
@@ -17,6 +32,40 @@ export const GenerateAiThumbnail = inngest.createFunction(
         const {userEmail,refImage,userInput,faceImage} = await event.data
         //upload image to cloud // imagekit
 
+        const uploadImageUrls =  await step.run(
+          "UploadImage",
+          async()=>{
+         
+            if(refImage!=null){
+
+              const refImageUrl =  await imagekit.upload({
+                file:refImage?.buffer??'',
+                fileName:refImage.name,
+                isPublished:true,
+                useUniqueFileName:false
+  
+  
+              })
+              // const faceImageUrl =  await imagekit.upload({
+              //   file:faceImage?.buffer??'',
+              //   fileName:faceImage.name,
+              //   isPublished:true,
+              //   useUniqueFileName:false
+  
+  
+              // })
+  
+              return {
+                refImageUrl:refImageUrl.url,
+                // faceImageUrl:faceImageUrl.url,
+  
+              }
+            }else{
+              return null
+            }
+          }
+        )
+
         // generate ai prompt from ai model 
 
         //generate ai image
@@ -24,6 +73,6 @@ export const GenerateAiThumbnail = inngest.createFunction(
         //save iamge to cloud
 
         //save record to databse
-        return userEmail
+        return uploadImageUrls
   }
 )
